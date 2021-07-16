2b39515bc8ae3c2cd4cb5b8646aff1b9ae5cc516
Aadhith-Ujesh <ujeshm2k2@gmail.com>
2021-07-16 23:36:27 +0530

    logim

--- staffstudent/src/main/java/com/login/Login.java
+++ staffstudent/src/main/java/com/login/Login.java
@@ -5,39 +5,47 @@
  */
 package com.login;
 
+import com.login.dao.LoginDao;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 public class Login extends HttpServlet {
-
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException{
 
     String user = request.getParameter("uname");
     String pass = request.getParameter("password");
-    if(user.equals("aad") && pass.equals("123"))
-    {
-        HttpSession session=request.getSession();
-        session.setAttribute("username",user);
-        response.sendRedirect("assignment.jsp");
-    }
-    else if(user.equals("teach") && pass.equals("123"))
-    {
-        HttpSession session=request.getSession();
-        session.setAttribute("username",user);
-        response.sendRedirect("tassignment.jsp");
-    }
-    else
-    {
-        response.sendRedirect("login.jsp");
-    }
+    
+    LoginDao dao = new LoginDao();
+    
+        try {
+            if(dao.check(user,pass))
+            {
+                HttpSession session=request.getSession();
+                session.setAttribute("username",user);
+                response.sendRedirect("assignment.jsp");
+            }
+//    else if(user.equals("teach") && pass.equals("123"))
+//    {
+//        HttpSession session=request.getSession();
+//        session.setAttribute("username",user);
+//        response.sendRedirect("tassignment.jsp");
+//    }
+            else
+            {
+                response.sendRedirect("login.jsp");
+            }   } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     
 }
--- staffstudent/src/main/java/com/login/dao/LoginDao.java
+++ staffstudent/src/main/java/com/login/dao/LoginDao.java
@@ -5,13 +5,44 @@
  */
 package com.login.dao;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author USERi
  */
-public class LoginDao {
-    public boolean check(String uname,String pass)
+public class LoginDao 
+{
+    String sql = "select * from clogin where user=? and pass=?";
+    String url = "jdbc:mysql//localhost:3306/login";
+    String username = "root";
+    String password = "Vishak1@3";
+    public boolean check(String uname,String pass) throws SQLException
     {
-        
+        try {
+            System.out.println("hi");
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url,username,password);
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, uname);
+            st.setString(2, pass);
+            ResultSet rs = st.executeQuery();
+            System.out.println(rs);
+//            st.close();
+//            rs.close();
+            if(rs.next())
+            {
+                return true;
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 }
